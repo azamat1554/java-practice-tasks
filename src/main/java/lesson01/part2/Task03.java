3f20b3f110930d1145201088d4215b2365060962
Azamat Abidokov <Azamat_Abidokov@epam.com>
2019-10-15 00:53:19 +0300

    Solved task3

--- src/main/java/lesson01/part2/Task03.java
+++ src/main/java/lesson01/part2/Task03.java
@@ -23,6 +23,7 @@ public class Task03 {
     }
 
     public static void hackSalary(int a) {
+        System.out.printf("Твоя зарплата составляет: %s долларов в месяц.", a+100);
         // напишите тут ваш код
     }
 }
